5d691c50aef256c25424c717e5652747a13f0752
Aarizia <u2469964@rts-365.dk>
2025-03-10 10:19:21 +0100

    Add new project entry for "Din Mægler" in ProjectsMain component

--- src/components/ProjectsMain.jsx
+++ src/components/ProjectsMain.jsx
@@ -16,12 +16,21 @@ export default function ProjectsMain() {
                 <ul className="projects__list">
                     <li className="project">
                         <details ref={detailsElement} class="project__details" /* onToggle={toggleHandler} */>
+                            <summary class="project__summary">Feb 2025: Din Mægler</summary>
+                            <div class="project__container">
+                                <p class="project__text">A web application for mobile phones browsing residential properties for sale. <span className='line-break'>The web app fetches data from an external api and is build in React, Vite, JavaScript and SASS.</span></p>
+                                <a href="https://praktik-din-m-gler-emmal.onrender.com" target="_blank" class="project__link">Open project website</a>
+                            </div>
+                        </details>
+                    </li>
+                    <li className="project">
+                        <details ref={detailsElement} class="project__details" /* onToggle={toggleHandler} */>
                             <summary class="project__summary">Jan 2025: MyMovies</summary>
                             <div class="project__container">
                                 <p class="project__text">A web application for mobile phones browsing current movies and tv-series. <span className='line-break'>The web app fetches data from an external api and is build in React, Vite, JavaScript and SASS.</span></p>
                                 <a href="https://projekt-moviez-aarizia.onrender.com" target="_blank" class="project__link">Open project website</a>
                             </div>
-                    </details>
+                        </details>
                     </li>
                     <li className="project">
                         <details ref={detailsElement} class="project__details" /* onToggle={toggleHandler} */>
